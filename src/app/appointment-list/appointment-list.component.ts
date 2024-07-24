debff57a6ef3a81e60195e23a580235c9112465d
Naveen Krishnan <krish.venkat@outlook.com>
2024-07-24 11:43:18 -0400

    second commit

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css'
+})
+export class AppointmentListComponent {
+
+  newAppointmentTitle : string = ""
+  newAppointmentDate : Date = new Date()
+
+  appointments:Appointment[] = []
+
+  addAppointment(){
+
+    alert(this.newAppointmentTitle + ',' + this.newAppointmentDate)
+  }
+  
+  
+
+}
